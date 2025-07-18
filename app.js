8c2734d2ddbbb6cf3ff30794cc6121f2ef32bfeb
megha-2461 <meghagoswami1624@gmail.com>
2025-07-19 00:01:34 +0530

    user signup and login

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express=require('express');
+const express = require('express');
 const app=express();
 const mongoose=require('mongoose');
 
@@ -20,9 +20,14 @@ app.use(express.urlencoded({extended : true})) //to parse data
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
-const listings = require("./routes/listing.js")
-const reviews = require("./routes/review.js")
+const listingsRouter = require("./routes/listing.js")
+const reviewsRouter = require("./routes/review.js")
+const userRouter = require("./routes/user.js")
+
 
 const   MONGO_URL="mongodb://127.0.0.1:27017/staydb"
 main()
@@ -55,15 +60,35 @@ app.get("/", (req, res)=>{
 app.use(session(sessionOptions));
 app.use(flash()); //must be before routes
 
+//after using session, we will use passportt
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser()); 
+passport.deserializeUser(User.deserializeUser()); 
+
+
 app.use((req, res, next)=>{
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   // console.log(res.locals.success);
   next();
 })
+
+/* app.get("/demouser", async (req, res)=>{
+let fakeUser = new User({
+email: "student@gmail.com",
+username: "student"
+})
+let registeredUser = await User.register(fakeUser, "helloworld"); //{user, pwd} this method checks automatically if usrname is unique or not
+res.send(registeredUser);
+}); */
+
+
  
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+app.use("/listings", listingsRouter);
+app.use("/listings/:id/reviews", reviewsRouter);
+app.use("/", userRouter);
 
 
 
